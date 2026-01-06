c851923ee61de0687c0c04814eee0607aa89b168
Stuart Romanek <stuart@apostrophecms.com>
2026-01-06 15:02:45 -0500

    include dist

---
+++ dist/src/packages/hint/dataAttributes.d.ts
@@ -0,0 +1,5 @@
+export declare const dataHintAttribute = "data-hint";
+export declare const dataStepAttribute = "data-step";
+export declare const dataHintPositionAttribute = "data-hint-position";
+export declare const dataTooltipClassAttribute = "data-tooltip-class";
+export declare const dataHintAnimationAttribute = "data-hint-animation";
